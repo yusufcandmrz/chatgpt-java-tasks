5d4654f1cbbf17a39953aa1bab97f06ab45bb895
yusufcandmrz <93606208+yusufcandmrz@users.noreply.github.com>
2025-08-28 23:00:03 +0300

    add calculateAverage, findTopStudent, searchStudent methods to StudentManager

--- src/main/java/yusufcandmrz/studentgrademanager/StudentManager.java
+++ src/main/java/yusufcandmrz/studentgrademanager/StudentManager.java
@@ -2,7 +2,9 @@ package yusufcandmrz.studentgrademanager;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
+
 
 public class StudentManager {
 
@@ -24,7 +26,7 @@ public class StudentManager {
                 7. Exit""";
         int operationNumber;
 
-        try(Scanner scanner = new Scanner(System.in)) {
+        try (Scanner scanner = new Scanner(System.in)) {
             while (true) {
                 System.out.println(operationList);
                 operationNumber = scanner.nextInt();
@@ -44,17 +46,51 @@ public class StudentManager {
     }
 
     private void addGradeToStudent(int id, int grade) {
-        for (Student student : studentList) {
-            if (student.getId() == id) {
-                student.getGrades().add(grade);
-                System.out.println("Grade added successfully");
-            }
-        }
+        searchStudent(id).ifPresentOrElse(
+                student -> {
+                    student.getGrades().add(grade);
+                    System.out.println("Grade added successfully");
+                },
+                () -> System.out.println("Student not found!")
+        );
     }
 
     private void showAllStudents() {
         for (Student student : studentList) {
             System.out.println(student);
         }
+    }
+
+    private double calculateAverage(int id) {
+        Student student = searchStudent(id).orElseThrow(() -> new IllegalArgumentException("Student not found!"));
+        return student.getGrades().stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0.0);
+    }
+
+    private Student findTopStudent() {
+        if (studentList.isEmpty()) {
+            return null;
+        }
+
+        Student topStudent = studentList.get(0);
+        double highestAverage = calculateAverage(topStudent.getId());
+
+        for (int i = 1; i < studentList.size(); i++) {
+            Student currentStudent = studentList.get(i);
+            double currentStudentAverage = calculateAverage(currentStudent.getId());
+            if (currentStudentAverage > highestAverage) {
+                highestAverage = currentStudentAverage;
+                topStudent = currentStudent;
+            }
+        }
+        return topStudent;
+    }
+
+    private Optional<Student> searchStudent(int id) {
+        return studentList.stream()
+                .filter(student -> student.getId() == id)
+                .findFirst();
     }
 }
