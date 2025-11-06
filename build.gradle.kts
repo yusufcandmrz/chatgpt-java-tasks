0ae0774e6d5a82c4bb70f9f709dabac51f3efe2b
yusufcandmrz <93606208+yusufcandmrz@users.noreply.github.com>
2025-11-06 23:25:13 +0300

    Add dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,12 @@ repositories {
 }
 
 dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.42")
+    annotationProcessor("org.projectlombok:lombok:1.18.42")
+
+    testCompileOnly("org.projectlombok:lombok:1.18.42")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.42")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
